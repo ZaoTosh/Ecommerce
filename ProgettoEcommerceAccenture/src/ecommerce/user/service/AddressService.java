1504ff45257136587ba0a1de09829a14df7eff40
MicheleOcchipinti <47862950+MicheleOcchipinti@users.noreply.github.com>
2020-07-17 10:11:33 +0200

    Aggiornamento medoto addAddress di AddressService

--- ProgettoEcommerceAccenture/src/ecommerce/user/service/AddressService.java
+++ ProgettoEcommerceAccenture/src/ecommerce/user/service/AddressService.java
@@ -6,11 +6,16 @@ public class AddressService {
 	private IndirizzoDaoImpl dao = new IndirizzoDaoImpl();
 	
 	public boolean addAddress(IndirizzoBean address) {
-		if(dao.addIndirizzo(address))
-			return true;
-		else
-			return false;
-				
+		String usernameInput = address.getUtente();
+		IndirizzoBean ultimoIndirizzoUtilizzato =  getUltimoIndirizzoByUser(usernameInput);
+		if(ultimoIndirizzoUtilizzato == null) {
+			return dao.addIndirizzo(address);
+		}
+		else {
+			ultimoIndirizzoUtilizzato.setUltimoIndirizzo("N");
+			dao.updateIndirizzo(ultimoIndirizzoUtilizzato);
+			return dao.addIndirizzo(address);
+		}			
 	}
 	public boolean modifyAddress(IndirizzoBean address) {
 		if(dao.updateIndirizzo(address))
