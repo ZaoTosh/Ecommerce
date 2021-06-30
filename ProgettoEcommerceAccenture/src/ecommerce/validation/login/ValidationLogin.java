bc02b846e18bdec84e65870a00b97b517ef69ea1
Paolo <50113488+paolinux97@users.noreply.github.com>
2021-06-30 11:51:53 +0200

    Update ValidationLogin.java
    modificato PASSWORD_PATTERN

--- ProgettoEcommerceAccenture/src/ecommerce/validation/login/ValidationLogin.java
+++ ProgettoEcommerceAccenture/src/ecommerce/validation/login/ValidationLogin.java
@@ -10,14 +10,15 @@ public class ValidationLogin {
 	private Pattern pattern;
     private Matcher matcher;
     
-    private static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z]).{8,16})";
+    private static final String PASSWORD_PATTERN = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,16}";
+    //private static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z]).{8,16})";
     //private static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*[0-9]))";
+    
     public ValidationLogin() {
         pattern = Pattern.compile(PASSWORD_PATTERN);
     }
  
     public boolean validatePassword(final String password) {
- 
         matcher = pattern.matcher(password);
         return matcher.matches();
  
